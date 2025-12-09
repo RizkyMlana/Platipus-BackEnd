3b3b588f06c831d59bc16611482f21cae443a104
RizkyMaulanaS <rizkyzky345@gmail.com>
2025-12-09 14:47:36 +0700

    Update: fixing route

--- src/routes/proposalRoutes.js
+++ src/routes/proposalRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import {authMiddleware, roleMiddleware} from '../middlewares/index.js';
+import { authMiddleware} from '../middlewares/authMiddleware.js';
+import { roleMiddleware } from "../middlewares/roleMiddleware.js";
 import { createProposal, getFastTrackProposals, feedbackProposal} from '../controllers/proposalController.js';
 
 const router = Router();
@@ -7,3 +8,5 @@ const router = Router();
 router.post('/', authMiddleware, roleMiddleware('EO', createProposal));
 router.get('/fastrack', authMiddleware, getFastTrackProposals);
 router.put('/:id/feedback', authMiddleware, roleMiddleware('SPONSOR', feedbackProposal));
+
+export default router;
