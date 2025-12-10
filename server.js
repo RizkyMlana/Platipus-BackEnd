8195fbf1b7127bac7d7e27b23d0bff8fb99df460
RizkyMaulanaS <rizkyzky345@gmail.com>
2025-12-10 11:18:02 +0700

    Update: fix multer

--- server.js
+++ server.js
@@ -5,7 +5,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-console.log("ENV CHECK: URL =", process.env.SUPABASE_URL)
-console.log("ENV CHECK: URL =", process.env.SUPABASE_SERVICE_ROLE_KEY)
 
 app.listen(PORT, () => console.log('Server Running '));
