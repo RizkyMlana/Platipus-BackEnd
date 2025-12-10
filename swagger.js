76cb3ceea3723685826b9cd7fa804babcab9cc2e
RizkyMaulanaS <rizkyzky345@gmail.com>
2025-12-10 18:39:29 +0700

    test

--- swagger.js
+++ swagger.js
@@ -27,7 +27,7 @@ const options = {
       { bearerAuth: [] }
     ],
   },
-  apis: [path.join("./src/controllers/*.js")],
+  apis: [path.join("/src/controllers/*.js")],
 };
 
 const specs = swaggerJsdoc(options);
