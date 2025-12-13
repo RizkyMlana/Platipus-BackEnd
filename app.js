daebd3c4720ee0b9701a2b38648ad000ec54f76f
RizkyMaulanaS <rizkyzky345@gmail.com>
2025-12-13 16:49:51 +0700

    push

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ import dotenv from 'dotenv';
 import router from './src/routes/index.js';
 import { specs } from "./swagger.js";
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+    origin: [
+        'http://localhost.5173',
+        'https://platipus-back-end.vercel.app/'
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
