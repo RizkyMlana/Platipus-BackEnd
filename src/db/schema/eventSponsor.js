eb8a717fdb97a50675f9b7dd13c1bd664ae79485
RizkyMaulanaS <rizkyzky345@gmail.com>
2025-12-16 14:24:25 +0700

    test

---
+++ src/db/schema/eventSponsor.js
@@ -0,0 +1,11 @@
+import {pgTable, uuid, text, varchar, timestamp,} from 'drizzle-orm/pg-core';
+
+export const eventSponsors = pgTable("event_sponsors", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  event_id: uuid("event_id").references(() => events.id).notNull(),
+  sponsor_id: uuid("sponsor_id").references(() => sponsorProfiles.id).notNull(),
+  submission_type: varchar("submission_type", { length: 20 }).notNull(),
+  status: varchar("status", { length: 20 }).default("PENDING"), 
+  feedback: text("feedback"),
+  created_at: timestamp("created_at").defaultNow(),
+});
--- src/db/schema/index.js
+++ src/db/schema/index.js
@@ -2,3 +2,4 @@ export * from './users.js';
 export * from './events.js';
 export * from './payments.js';
 export * from './masterTable.js';
+export * from './eventSponsor.js';
